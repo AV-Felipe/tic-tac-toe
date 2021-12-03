05dcf7e07c5d255e42fe73e33c59afce68ec881a
AV-Felipe <aerogrado@gmail.com>
2021-12-03 09:54:04 -0300

    Add machine player

--- script.js
+++ script.js
@@ -21,10 +21,6 @@ const boardFields = [[l1c1, l1c2, l1c3], [l2c1, l2c2, l2c3], [l3c1, l3c2, l3c3]]
 
 // LISTENERS
 
-//boardFields.forEach(a => {
-//    a.addEventListener('click', fieldSelector);    
-//});
-
 // FUNCTIONS
 
 function fieldSelector(l, c) {
@@ -32,16 +28,27 @@ function fieldSelector(l, c) {
         boardFields[l][c].innerHTML = 'O';
         boardFields[l][c].setAttribute('disabled', '');
         gameBoard[l][c] = 'o';
-        isWinner('o');
-        turn = 'x';
-        console.log(gameBoard[l][c]);
+        let winnerFlag = isWinner('o');
+        if(!winnerFlag){
+            turn = 'x';
+            console.log(gameBoard[l][c]);
+            machinePlay ()
+        }else{
+            turn = 'end';
+        }
+        
     }else{
         boardFields[l][c].innerHTML = 'X';
         boardFields[l][c].setAttribute('disabled', '');
         gameBoard[l][c] = 'x';
-        isWinner('x');
-        turn = 'o';
-        console.log(gameBoard[l][c]);
+        let winnerFlag = isWinner('x');
+        if(!winnerFlag){
+            turn = 'o';
+            console.log(gameBoard[l][c]);
+        }else{
+            turn = 'end';
+        }
+
     }
 }
 
@@ -76,14 +83,60 @@ function isWinner(player) {
                     b.setAttribute('disabled', '');
                 });    
             });
-            
+
             l1c1.style.background = 'black';
             l1c3.style.background = 'black';
             l2c2.style.background = 'black';
             l3c1.style.background = 'black';
             l3c3.style.background = 'black';
         }
+        return(true);
     }else{
         console.log('e agora?')
+        return(false);
     }
+}
+
+function machinePlay () {
+    let options = [...gameBoard];
+    console.log(options);
+    
+    let rowOption;
+    let againFlag = true;
+    
+    while (againFlag){
+    rowOption = Math.floor(Math.random() * options.length);
+    console.log(rowOption); // ou 0 ou 1 ou 2
+    
+    
+    options[rowOption].forEach(a => {
+        
+        console.log(options[a]);
+        if(againFlag === true){
+            if(a != 'o' && a != 'x'){
+                againFlag = false;
+            }
+        };
+        
+    });
+
+    };
+
+    let columnOption = 0;
+    //debugger;
+    while(options[rowOption][columnOption] === 'o' || options[rowOption][columnOption] === 'x'){
+        //debugger;
+        columnOption++;
+    }
+
+    boardFields[rowOption][columnOption].innerHTML = 'X';
+    boardFields[rowOption][columnOption].setAttribute('disabled', '');
+    gameBoard[rowOption][columnOption] = 'x';
+    let winnerFlag = isWinner('x');
+    if(!winnerFlag){
+        turn = 'o';
+    }else{
+        turn = 'end';
+    }
+    
 }
